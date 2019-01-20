e4b2e6828cf73edda6d1353f501b87651af20589
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-01-20 17:07:20 +0100

    Added the selected dishes and prices on the left panel

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -12,10 +12,10 @@
  * @param {Object} model - the reference to the Dinner Model
  */ 
 var SelectDishView = function (container, model) {
+    //Search result
     var searchResultContainer = container.find("#searchResultContainer");
 
     result = model.getAllDishes('starter');
-    
     result.map(function(dish) {
         var image = $('<img />').attr({
             'id': 'myImage'+dish.id,
@@ -32,6 +32,39 @@ var SelectDishView = function (container, model) {
             "class": 'col-md-3',
             }).append(image, imageText))
     })
+
+    //Number of guests input on left panel
+    numberOfGuestsInput = container.find('#numberOfGuestsInput');
+    numberOfGuestsInput.attr('value', model.getNumberOfGuests());
+
+    //Temporary add dishes to the menu
+    model.addDishToMenu(1);
+    model.addDishToMenu(101);
+
+    //Selected dishes in left panel
+    var selectedDishesContainer = container.find('#selectedDishesContainer');
+    var selectedDishes = model.getFullMenu();
+
+    selectedDishes.map(function(dish) {
+        var oneSelectedDishContainer = $('<div/>').attr({
+            'class': 'flex flex-row justify-between bg-primary margin-2',
+        })
+
+        var nameOfDish = $('<div/>').attr('class', "padding-2 flex-1").html(dish.name)
+        var priceOfDish = $('<div/>').attr('class', "padding-2 flex-1 text-right").html(model.getDishPrice(dish.id))
+
+        oneSelectedDishContainer.append(nameOfDish, priceOfDish);
+        selectedDishesContainer.append(oneSelectedDishContainer);
+    });
+
+    //Total price on left panel
+    var totalPriceContainer = $('<div/>').attr({
+        'class': 'flex flex-row justify-between margin-2',
+    })
+    var totalPrice =  $('<div/>').attr('class', "padding-2 flex-1").html(model.getTotalMenuPrice());
+    totalPriceContainer.append(totalPrice);
+    selectedDishesContainer.append(totalPriceContainer);
+
 	
 }
  
