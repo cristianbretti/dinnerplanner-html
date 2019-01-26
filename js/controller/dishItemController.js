d7ee61d346ad8d6810e5c77315309381e456ced2
Anton Stagge <antonstagge95@gmail.com>
2019-01-26 12:24:05 +0100

    remove app from dishItemcontrollers

--- js/controller/dishItemController.js
+++ js/controller/dishItemController.js
@@ -3,7 +3,7 @@
  * @param {Object} dishItemView - the dishItemView object
  * @param {Object} model - the reference to the Dinner Model.
  */ 
-var DishItemController = function (dishItemView, model, app) {
+var DishItemController = function (dishItemView, model) {
     dishItemView.item.click(function() {
         model.setDetailedDinner(dishItemView.dishId);
         showDishDetailsScreen();
